409d0928f0597386cf63ce94239c582d7ca6af55
Everton Leão <everleao@gmail.com>
2023-08-28 21:50:57 -0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {IndexModule} from './pages/index/index.module';
+import {PortifolioModule} from './pages/portifolio/portifolio.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+
+    IndexModule,
+    PortifolioModule,
     BrowserModule,
     AppRoutingModule
   ],
